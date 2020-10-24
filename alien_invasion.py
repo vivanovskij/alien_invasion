e55cac1b9d407e079483f40d39a5f27339645b71
Виталий Ивановский <vivanovskijj@gmail.com>
2020-10-24 22:53:27 +0300

    refactoring alien_invasion run_game

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,15 +20,21 @@ class AlienInvasion:
         """Запуск основного цикла игры."""
         while True:
             # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._check_events()
+            self._update_screen()
 
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
