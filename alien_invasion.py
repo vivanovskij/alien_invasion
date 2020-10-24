ce388175ce7d1c3d6a3be333672ffb50211a849b
Виталий Ивановский <vivanovskijj@gmail.com>
2020-10-24 23:08:48 +0300

    moving ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         while True:
             # Отслеживание событий клавиатуры и мыши.
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -30,8 +31,14 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Переместить корабль вправо.
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
