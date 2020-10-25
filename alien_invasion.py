ebecbff4d6dc433f1ef6cf9cbb7c751f255ea982
Виталий Ивановский <vivanovskijj@gmail.com>
2020-10-25 17:51:51 +0200

    game stats

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,9 +36,10 @@ class AlienInvasion:
         while True:
             # Отслеживание событий клавиатуры и мыши.
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
